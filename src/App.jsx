009e53ebd06c4fc82fa350900b9d787147e7e27a
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-03-26 23:54:51 +0530

    main and money pyramid ui made

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,18 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div>Hello kbc</div>;
+  return (
+    <div className="app">
+      <div className="main">main</div>
+      <div className="pyramid">
+        <div className="moneyList">
+          <div className="moneyListItem active">
+            <span className="moneyListItemNumber">4</span>
+            <span className="moneyListItemAmount">Rs 400</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
 export default App;
