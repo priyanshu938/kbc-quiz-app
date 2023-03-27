bd7711ceb1b6f18323b9500721e4c5fc8a07642b
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-03-27 20:28:21 +0530

    kbc game made

--- src/App.jsx
+++ src/App.jsx
@@ -1,99 +1,35 @@
 import { useEffect, useState, useMemo } from "react";
 
 import "./App.css";
+import Timer from "./components/Timer";
 import Trivia from "./components/Trivia";
+import Start from "./components/Start";
+import questionsData from "./questionsData";
 
 function App() {
+  const [username, setUsername] = useState(null);
   const [questionNumber, setQuestionNumber] = useState(1);
   const [stop, setStop] = useState(false);
   const [earned, setEarned] = useState("Rs 0");
-  const data = [
-    {
-      id: 1,
-      question: "Rolex is a company that specializes in what type of product?",
-      answers: [
-        {
-          text: "Phone",
-          correct: false,
-        },
-        {
-          text: "Watches",
-          correct: true,
-        },
-        {
-          text: "Food",
-          correct: false,
-        },
-        {
-          text: "Cosmetic",
-          correct: false,
-        },
-      ],
-    },
-    {
-      id: 2,
-      question: "When did the website `Facebook` launch?",
-      answers: [
-        {
-          text: "2004",
-          correct: true,
-        },
-        {
-          text: "2005",
-          correct: false,
-        },
-        {
-          text: "2006",
-          correct: false,
-        },
-        {
-          text: "2007",
-          correct: false,
-        },
-      ],
-    },
-    {
-      id: 3,
-      question: "Who played the character of harry potter in movie?",
-      answers: [
-        {
-          text: "Johnny Deep",
-          correct: false,
-        },
-        {
-          text: "Leonardo Di Caprio",
-          correct: false,
-        },
-        {
-          text: "Denzel Washington",
-          correct: false,
-        },
-        {
-          text: "Daniel Red Cliff",
-          correct: true,
-        },
-      ],
-    },
-  ];
-
+  const data = questionsData;
   const moneyPyramid = useMemo(
     () =>
       [
-        { id: 1, amount: "Rs 100" },
-        { id: 2, amount: "Rs 200" },
-        { id: 3, amount: "Rs 300" },
-        { id: 4, amount: "Rs 500" },
-        { id: 5, amount: "Rs 1000" },
-        { id: 6, amount: "Rs 2000" },
-        { id: 7, amount: "Rs 4000" },
-        { id: 8, amount: "Rs 8000" },
-        { id: 9, amount: "Rs 16000" },
-        { id: 10, amount: "Rs 32000" },
-        { id: 11, amount: "Rs 64000" },
-        { id: 12, amount: "Rs 125000" },
-        { id: 13, amount: "Rs 250000" },
-        { id: 14, amount: "Rs 500000" },
-        { id: 15, amount: "Rs 1000000" },
+        { id: 1, amount: "Rs 1000" },
+        { id: 2, amount: "Rs 2000" },
+        { id: 3, amount: "Rs 3000" },
+        { id: 4, amount: "Rs 5000" },
+        { id: 5, amount: "Rs 10000" },
+        { id: 6, amount: "Rs 20000" },
+        { id: 7, amount: "Rs 40000" },
+        { id: 8, amount: "Rs 80000" },
+        { id: 9, amount: "Rs 160000" },
+        { id: 10, amount: "Rs 320000" },
+        { id: 11, amount: "Rs 640000" },
+        { id: 12, amount: "Rs 1250000" },
+        { id: 13, amount: "Rs 2500000" },
+        { id: 14, amount: "Rs 5000000" },
+        { id: 15, amount: "Rs 10000000" },
       ].reverse(),
     []
   );
@@ -104,43 +40,51 @@ function App() {
   }, [moneyPyramid, questionNumber]);
   return (
     <div className="app">
-      <div className="main">
-        {stop ? (
-          <h1 className="endText">You earned: {earned} </h1>
-        ) : (
-          <>
-            <div className="top">
-              <div className="timer">30</div>
-            </div>
+      {username ? (
+        <>
+          <div className="main">
+            {stop ? (
+              <h1 className="endText">You earned: {earned} </h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer setStop={setStop} questionNumber={questionNumber} />
+                  </div>
+                </div>
 
-            <div className="bottom">
-              <Trivia
-                data={data}
-                setStop={setStop}
-                questionNumber={questionNumber}
-                setQuestionNumber={setQuestionNumber}
-              />
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    setStop={setStop}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <div className="moneyList">
+              {moneyPyramid.map((m, i) => (
+                <li
+                  key={i}
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
             </div>
-          </>
-        )}
-      </div>
-      <div className="pyramid">
-        <div className="moneyList">
-          {moneyPyramid.map((m, i) => (
-            <li
-              key={i}
-              className={
-                questionNumber === m.id
-                  ? "moneyListItem active"
-                  : "moneyListItem"
-              }
-            >
-              <span className="moneyListItemNumber">{m.id}</span>
-              <span className="moneyListItemAmount">{m.amount}</span>
-            </li>
-          ))}
-        </div>
-      </div>
+          </div>
+        </>
+      ) : (
+        <Start setUsername={setUsername} />
+      )}
     </div>
   );
 }
---
+++ src/questionsData.js
@@ -0,0 +1,334 @@
+const questionsData = [
+  {
+    id: 1,
+    question: "Rolex is a company that specializes in what type of product?",
+    answers: [
+      {
+        text: "Phone",
+        correct: false,
+      },
+      {
+        text: "Watches",
+        correct: true,
+      },
+      {
+        text: "Food",
+        correct: false,
+      },
+      {
+        text: "Cosmetic",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "When did the website `Facebook` launch?",
+    answers: [
+      {
+        text: "2004",
+        correct: true,
+      },
+      {
+        text: "2005",
+        correct: false,
+      },
+      {
+        text: "2006",
+        correct: false,
+      },
+      {
+        text: "2007",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "Who played the character of Harry Potter in the movie?",
+    answers: [
+      {
+        text: "Johnny Deep",
+        correct: false,
+      },
+      {
+        text: "Leonardo Di Caprio",
+        correct: false,
+      },
+      {
+        text: "Denzel Washington",
+        correct: false,
+      },
+      {
+        text: "Daniel Radcliffe",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is the largest planet in our solar system?",
+    answers: [
+      {
+        text: "Jupiter",
+        correct: true,
+      },
+      {
+        text: "Saturn",
+        correct: false,
+      },
+      {
+        text: "Mars",
+        correct: false,
+      },
+      {
+        text: "Earth",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Who wrote the famous novel 'To Kill a Mockingbird'?",
+    answers: [
+      {
+        text: "Harper Lee",
+        correct: true,
+      },
+      {
+        text: "J.K. Rowling",
+        correct: false,
+      },
+      {
+        text: "Stephen King",
+        correct: false,
+      },
+      {
+        text: "George Orwell",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "What is the capital of Australia?",
+    answers: [
+      {
+        text: "Sydney",
+        correct: false,
+      },
+      {
+        text: "Melbourne",
+        correct: false,
+      },
+      {
+        text: "Canberra",
+        correct: true,
+      },
+      {
+        text: "Brisbane",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "What is the largest organ in the human body?",
+    answers: [
+      {
+        text: "Liver",
+        correct: false,
+      },
+      {
+        text: "Heart",
+        correct: false,
+      },
+      {
+        text: "Brain",
+        correct: false,
+      },
+      {
+        text: "Skin",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "What is the name of the largest ocean on Earth?",
+    answers: [
+      {
+        text: "Atlantic Ocean",
+        correct: false,
+      },
+      {
+        text: "Indian Ocean",
+        correct: false,
+      },
+      {
+        text: "Arctic Ocean",
+        correct: false,
+      },
+      {
+        text: "Pacific Ocean",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "Which country has the largest population in the world?",
+    answers: [
+      {
+        text: "India",
+        correct: false,
+      },
+      {
+        text: "United States",
+        correct: false,
+      },
+      {
+        text: "China",
+        correct: true,
+      },
+      {
+        text: "Russia",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "What is the name of the highest mountain in the world?",
+    answers: [
+      {
+        text: "Mount Everest",
+        correct: true,
+      },
+      {
+        text: "Mount Kilimanjaro",
+        correct: false,
+      },
+      {
+        text: "Mount McKinley",
+        correct: false,
+      },
+      {
+        text: "Mount Fuji",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "What is the chemical symbol for gold?",
+    answers: [
+      {
+        text: "Ag",
+        correct: false,
+      },
+      {
+        text: "Au",
+        correct: true,
+      },
+      {
+        text: "Cu",
+        correct: false,
+      },
+      {
+        text: "Fe",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "Which animal is the largest on Earth?",
+    answers: [
+      {
+        text: "Elephant",
+        correct: false,
+      },
+      {
+        text: "Giraffe",
+        correct: false,
+      },
+      {
+        text: "Blue Whale",
+        correct: true,
+      },
+      {
+        text: "Hippopotamus",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "Which planet in our solar system is known as the 'Red Planet'?",
+    answers: [
+      {
+        text: "Venus",
+        correct: false,
+      },
+      {
+        text: "Jupiter",
+        correct: false,
+      },
+      {
+        text: "Mars",
+        correct: true,
+      },
+      {
+        text: "Uranus",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "What is the smallest country in the world?",
+    answers: [
+      {
+        text: "Vatican City",
+        correct: true,
+      },
+      {
+        text: "Monaco",
+        correct: false,
+      },
+      {
+        text: "Maldives",
+        correct: false,
+      },
+      {
+        text: "Nauru",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "What is the name of the largest desert in the world?",
+    answers: [
+      {
+        text: "Sahara",
+        correct: true,
+      },
+      {
+        text: "Gobi",
+        correct: false,
+      },
+      {
+        text: "Arabian",
+        correct: false,
+      },
+      {
+        text: "Kalahari",
+        correct: false,
+      },
+    ],
+  },
+];
+
+export default questionsData;
